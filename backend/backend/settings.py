718efd6cca7f3844c2a735e47d9872fcf2de9a44
artemlenskih <artemlenskih@gmail.com>
2026-05-20 01:12:06 +0700

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,3 +101,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://youpipe-1.onrender.com']
