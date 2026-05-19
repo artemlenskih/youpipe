9f571c2effc1edaf3932860ae1d2a8811c0022f8
artemlenskih <artemlenskih@gmail.com>
2026-05-20 00:19:59 +0700

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('admin/')),
     path('admin/', admin.site.get_admin_urls() if hasattr(admin, 'get_admin_urls') else admin.site.urls),
     path('api/', include('api.urls')),
 ]
