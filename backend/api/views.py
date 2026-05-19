2b63127636c8b1c18aa02e1d31da0adc3f06684a
artemlenskih <artemlenskih@gmail.com>
2026-05-20 01:09:04 +0700

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,14 +6,16 @@ from django.contrib.auth.models import User
 from rest_framework import generics, status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Video, ChatMessage
 from .serializers import RegisterSerializer, VideoSerializer, ChatMessageSerializer
 
+@method_decorator(csrf_exempt, name='dispatch')
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (permissions.AllowAny,)
-    serializer_class = RegisterSerializer
+    serializer_class = RegisterSerialize
 
 
 class VideoListCreateView(generics.ListCreateAPIView):
