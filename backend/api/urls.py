09d01f30570bc094877425cf9ecd740249d990a1
artemlenskih <artemlenskih@gmail.com>
2026-05-20 09:34:13 +0700

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.http import JsonResponse
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView, VideoListCreateView, VideoStreamView, ChatMessageListCreateView, VideoDeleteView
 
+def api_root(request):
+    return JsonResponse({"status": "API is running"})
+
 urlpatterns = [
+    path('', api_root),
     path('auth/register/', RegisterView.as_view(), name='auth_register'),
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
